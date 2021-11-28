68dc8b81b3a598a5829907441a3c4e498961da38
Geoffrey Jarman <geoff@Geoffreys-MacBook-Pro.local>
2021-11-28 09:44:30 -0600

    gallery

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,77 +1,54 @@
-import React, { useEffect, useState } from "react";
+import media from "../assets/gallery/index.js"
 import Image from "next/image";
-import media from "../assets/gallery/index";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+import "swiper/css/thumbs"
+
 import styles from "../styles/Carousel.module.css"
 
+
+// import Swiper core and required modules
+import SwiperCore, {
+  FreeMode,Navigation,Thumbs
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([FreeMode,Navigation,Thumbs]);
+
+
 export default function Carousel() {
-  const [currentImage, setCurrentImage] = useState(0)
-  const [deck, setDeck] = useState([]);
-  const [full, setFull] = useState(false);
-  useEffect(() => {
-    let tempDeck = []
-    if (currentImage <= 2) {
-      setDeck([0,1,2,3,4]);
-    }
-    if (currentImage > 2) {
-      for(let i = currentImage - 3; i < currentImage + 2; i++) {
-        console.log(tempDeck)
-        tempDeck.push(i+1)
-      }
-      setDeck(tempDeck);
-      tempDeck = [];
-    }
-    if (currentImage > 19) {
-      setDeck([18,19,20,21,22]);
-    }
-    
-    
-  }, [currentImage]);
-  const handleTab = ({ target }) => {
-    console.log(target.name)
-    setCurrentImage(parseInt(target.name))
-  }
-  const increment = () => {
-    if (currentImage <= 21) {
-      setCurrentImage(currentImage + 1)
-    }
-  }
-  const decrement = () => {
-    if (currentImage > 0) {
-      setCurrentImage(currentImage - 1)
-    }
-  }
+  
+  
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  
+  
   return (
     <>
-      <div className={styles.carousel}>
-        <div className={styles.caroConsole}>
-          <div className={styles.btnBox}>
-            <div className={styles.leftBtn} onClick={decrement}/>
-          </div>
-          <div className={full ? styles.fullSize : styles.main}>
-            <div className={full ? styles.closeBtn : styles.expandBtn} onClick={() => setFull(!full)}/>
-            <Image src={media[currentImage]} alt={currentImage} />
-          </div>
-          <div className={styles.btnBox}>
-            <div className={styles.rightBtn}  onClick={increment}/>
-          </div>
-        </div>
-        <div className={styles.deck} onClick={handleTab}>
-          {deck && deck.map(itemNumber => {
-            return (
-              <>
-                <div key={itemNumber} className={itemNumber == currentImage ? styles.active : styles.inactive}>
-                  <Image 
-                    src={media[itemNumber]} 
-                    name={itemNumber} 
-                    layout="fill"
-                    objectFit="cover"
-                    alt={itemNumber} 
-                    onClick={handleTab} />
-                </div>
-              </>
-            )})}
-        </div>
-      </div>
+   <h1 style={{textAlign:"center",color:"black",fontSize:"3rem",marginBottom:"30px"}}>GALLERY</h1>
+  <Swiper style={{margin:"3%",marginTop:"0",marginBottom:"0",'--swiper-navigation-color': '#fff','--swiper-pagination-color': '#fff', backgroundColor:"#292929", padding:"30px", paddingBottom:"10px"}} spaceBetween={10} navigation={true} thumbs={{ swiper: thumbsSwiper }} className="mySwiper2">
+  {media.map(img => {
+        return (
+          <SwiperSlide style={{display:"flex",justifyContent:"center", height:"65vh"}}>
+            <Image src={img} layout="fill" objectFit="contain" alt="carpet" />
+          </SwiperSlide>
+        )
+      })}
+  </Swiper>
+  <Swiper style={{margin:"3%",marginTop:"0",marginBottom:"0",'--swiper-navigation-color': '#fff','--swiper-pagination-color': '#fff', backgroundColor:"#292929",padding:"3%",paddingTop:"0"}} onSwiper={setThumbsSwiper} spaceBetween={10} slidesPerView={4} freeMode={true} watchSlidesProgress={true} className="mySwiper">
+  {media.map(img => {
+        return (
+          <SwiperSlide style={{display:"flex",justifyContent:"center", height:"150px"}}>
+            <Image src={img} layout="fill" objectFit="cover" alt="carpet" />
+          </SwiperSlide>
+        )
+      })}
+  </Swiper>
     </>
   )
 }
